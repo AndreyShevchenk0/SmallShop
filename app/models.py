84111252dd1612bb4086c767da7d77b4063f63b7
AndreyS <endry.sh.83.v@gmail.com>
2021-01-28 02:28:11 +0200

    2 komit settings + dirs + urls

--- app/models.py
+++ app/models.py
@@ -13,6 +13,29 @@ User = get_user_model()
 # 6 Customer
 # 7 Specifications
 
+class LatestProductsManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model_in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(products, key=lambda x: x.__class__.meta.modul_name.startwith(with_respect_to),
+                                  reverse=True)
+        return products
+
+
+class LatestProducts:
+
+    object = LatestProductsManager()
+
 
 class Category(models.Model):
     """ модель категорii """
@@ -118,3 +141,5 @@ class Smartphone(Product):
 
     def __str__(self):
         return '{} : {}'.format(self.category.name, self.title)
+
+
---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    #path('', views.mod, name='base'),
+    #path('myJob/', views.xer, name='myJob'),
+    #path('customer/<srt:pk_test>/', views.costomer, name='costomer'),
+]
