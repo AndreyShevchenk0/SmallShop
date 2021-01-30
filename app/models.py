a03615dd3f2c197d83f0ec6b0c8df3e87f7baa52
AndreyS <endry.sh.83.v@gmail.com>
2021-01-30 22:10:09 +0200

    final

--- app/models.py
+++ app/models.py
@@ -5,13 +5,14 @@ from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes. fields import GenericForeignKey
 from django.urls import reverse
+from django.utils import timezone
 # from django.core.files.uploadedfile import InMemoryUploadedFile
 # from io import BytesIO
 
 User = get_user_model()
 
 def get_models_for_count(*model_names):
-    return [models.Count(model_names)for model_name in model_names]
+    return [models.Count(model_names) for model_name in model_names]
 
 # функция преобразующая все модели Products
 # аналог get_absolut_url
@@ -20,12 +21,12 @@ def get_product_url(obj, viewname):
     return  reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
 
 
-class MinResolutionErrorException(Exception):
-    pass
-
-
-class MaxResolutionErrorException(Exception):
-    pass
+# class MinResolutionErrorException(Exception):
+#     pass
+#
+#
+# class MaxResolutionErrorException(Exception):
+#     pass
 
 # 1 Category
 # 2 Product
@@ -37,11 +38,12 @@ class MaxResolutionErrorException(Exception):
 
 class LatestProductsManager:
     """ менеджер модели """
+
     @staticmethod
     def get_products_for_main_page(*args, **kwargs):
         with_respect_to = kwargs.get('with_respect_to')
         products = []
-        ct_models = ContentType.objects.filter(model_in=args)
+        ct_models = ContentType.objects.filter(model__in=args)
         for ct_model in ct_models:
             model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
             products.extend(model_products)
@@ -55,12 +57,12 @@ class LatestProductsManager:
 
 
 class LatestProducts:
-    """ обьект менеджера модели """
-    object = LatestProductsManager()
+    """ обьект менеджера модели  делает имитацию модели для вивода на екран """
+    objects = LatestProductsManager()
 
 
 class CategoryManager(models.Manager):
-    """ млдель категорий менеджер """
+    """ модель категорий менеджер """
 
     CATEGORY_NAME_COUNT_NAME = {
         'Ноутбуки': 'notebook__count',
@@ -71,9 +73,13 @@ class CategoryManager(models.Manager):
         return super().get_queryset()
 
     def get_categories_for_left_sidebar(self):
-        models =get_models_for_count('notebook', 'smartphone')
-        qs = list(self.get_queryset().annotate(*models).values())
-        return [dict(name=c['name'], slug=c['slug'], count=c[self.CATEGORY_NAME_COUNT_NAME[c['name']]])for c in qs]
+        models = get_models_for_count('notebook', 'smartphone')
+        qs = list(self.get_queryset().annotate(*models))
+        data = [
+            dict(name=c.name, url=c.get_absolute_url(), count=getattr(c, self.CATEGORY_NAME_COUNT_NAME[c.name]))
+            for c in qs
+        ]
+        return data
 
 
 class Category(models.Model):
@@ -86,13 +92,16 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
 
 class Product(models.Model):
     """  абстрактна модель продукту для подальшого наслідування """
 
-    MIN_RESOLUTION = (400, 400)
-    MAX_RESOLUTION = (800, 800)
-    MAX_IMAGE_SIZE = 3145728
+    # MIN_RESOLUTION = (400, 400)
+    # MAX_RESOLUTION = (800, 800)
+    # MAX_IMAGE_SIZE = 3145728
 
     class Meta:
         abstract = True
@@ -106,6 +115,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_model_name(self):
+        return self.__class__.__name__.lower()
 
     # def save(self, *args, **kwargs):
     #     """ Пренудительне обрізання зображення """
@@ -151,14 +163,18 @@ class CartProduct(models.Model):
     def __str__(self):
         return 'Продукт: {} (для корзини)'.format(self.content_object.title)
 
+    def save(self, *args, **kwargs):
+        self.final_price = self.qty * self.content_object.price
+        super().save(*args, **kwargs)
+
 
 class Cart(models.Model):
     """ модель корзини """
 
-    owner = models.ForeignKey('Customer', verbose_name='власник', on_delete=models.CASCADE)
+    owner = models.ForeignKey('Customer', null=True, verbose_name='власник', on_delete=models.CASCADE)
     products = models.ManyToManyField(CartProduct, blank=True, related_name='related_car')
     total_products = models.PositiveIntegerField(default=0)  # для коректного отображения товара в корзине
-    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='загальна вартість')
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, default=0, verbose_name='загальна вартість')
     in_order = models.BooleanField(default=False)
     for_anonymous_user = models.BooleanField(default=False)
 
@@ -170,8 +186,9 @@ class Customer(models.Model):
     """ модель покупця """
 
     user = models.ForeignKey(User, verbose_name='користувач', on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20, verbose_name='номер телефона')
-    addres = models.CharField(max_length=255, verbose_name='адреса')
+    phone = models.CharField(max_length=20, null=True, blank=True, verbose_name='номер телефона')
+    address = models.CharField(max_length=255, null=True, blank=True, verbose_name='адреса')
+    orders = models.ManyToManyField('Order', related_name='related_customer', verbose_name='Замовлення покупця')
 
     def __str__(self):
         return 'Покупець: {} {}'.format(self.user.first_name, self.user.last_name)
@@ -226,3 +243,53 @@ class Smartphone(Product):
 
     def get_absolute_url(self):
         return get_product_url(self, 'product_detail')
+
+
+class Order(models.Model):
+    """ модель заказа покупателя """
+
+    STATUS_NEW = 'new'
+    STATUS_IN_PROGRESS = 'in_progress'
+    STATUS_READY = 'is_ready'
+    STATUS_COMPLETED = 'completed'
+
+    BUYING_TYPE_SELF = 'self'
+    BUYING_TYPE_DELIVERY = 'delivery'
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'Новый заказ'),
+        (STATUS_IN_PROGRESS, 'Заказ в обработке'),
+        (STATUS_READY, 'Заказ готов'),
+        (STATUS_COMPLETED, 'Заказ выполнен')
+    )
+
+    BUYING_TYPE_CHOICES = (
+        (BUYING_TYPE_SELF, 'Самовывоз'),
+        (BUYING_TYPE_DELIVERY, 'Доставка')
+    )
+
+    customer = models.ForeignKey(Customer, verbose_name='Покупатель', related_name='related_orders',
+                                 on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, verbose_name='Имя')
+    last_name = models.CharField(max_length=255, verbose_name='Фамилия')
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    cart = models.ForeignKey(Cart, verbose_name='Корзина', on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=1024, verbose_name='Адрес', null=True, blank=True)
+    status = models.CharField(
+        max_length=100,
+        verbose_name='Статус заказ',
+        choices=STATUS_CHOICES,
+        default=STATUS_NEW
+    )
+    buying_type = models.CharField(
+        max_length=100,
+        verbose_name='Тип заказа',
+        choices=BUYING_TYPE_CHOICES,
+        default=BUYING_TYPE_SELF
+    )
+    comment = models.TextField(verbose_name='Комментарий к заказу', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания заказа')
+    order_date = models.DateField(verbose_name='Дата получения заказа', default=timezone.now)
+
+    def __str__(self):
+        return str(self.id)
--- mainapp/settings.py
+++ mainapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static_dev'),
 )
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
